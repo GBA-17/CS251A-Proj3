de73d03332a36f2bea7c03219db8115778c46eb0
Gabriel Biren <gbiren@ucla.edu>
2021-02-24 20:48:03 +0000

    Add script to run the tests

---
+++ run_gem5.py
@@ -0,0 +1,16 @@
+import subprocess
+
+freqs = ['1GHz', '4GHz']
+benchmarks = ['merge', 'lfsr', 'mm', 'sieve', 'spmv']
+cache_repls = ['LIPRP', 'NMRURP', 'RandomRP']
+
+for prog_name in benchmarks:
+	prog_loc = "cs251a-microbench/" + prog_name
+	for repl in cache_repls:
+		assoc = 16 if repl == "RandomRP" else 8
+		freq = "2.2GHz" if repl == "RandomRP" else "2GHz"
+		inst = ("../gem5/build/X86/gem5.opt --outdir=CS251A-Proj3/%s_%s gem5/configs/example/se.py --cmd=%s --cpu-type=DerivO3CPU " +\
+			"--cpu-clock=%s --caches --l1d_size=64kB --l1i_size=64kB --l2cache --l2_size=2MB --mem-type=DDR3_1600_8x8 " +\
+			"--l1d_repl=%s --l2_repl=%s --l1d_assoc=%s --l2_assoc=%s") 
+		inst = inst % (prog_name, repl, prog_loc, freq, repl, repl, assoc, assoc)
+		subprocess.run(inst.split())
