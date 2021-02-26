bb3d2147c7621ebf8f8838f7c4435ce91e3bbc4b
Gabriel Biren <gbiren@ucla.edu>
2021-02-25 20:54:01 -0800

    More cleanup

--- freq.py
+++ freq.py
@@ -1,6 +1,5 @@
 import subprocess
 
-freqs = ['1GHz', '4GHz']
 benchmarks = ['merge', 'lfsr', 'mm', 'sieve', 'spmv']
 cache_repls = ['LIPRP', 'NMRURP', 'RandomRP']
 
@@ -9,7 +8,9 @@ for prog_name in benchmarks:
 	for repl in cache_repls:
 		assoc = 16 if repl == "RandomRP" else 8
 		freq = "2.2GHz" if repl == "RandomRP" else "2GHz"
-		inst = ("../gem5/build/X86/gem5.opt --outdir=CS251A-Proj3/%s_%s gem5/configs/example/se.py --cmd=%s --cpu-type=DerivO3CPU " +\
+		if repl == "LIPRP":
+			freq = "1.98GHz"
+		inst = ("../gem5/build/X86/gem5.opt --outdir=freq_tests/%s_%s ../gem5/configs/example/se.py --cmd=%s --cpu-type=DerivO3CPU " +\
 			"--cpu-clock=%s --caches --l1d_size=64kB --l1i_size=64kB --l2cache --l2_size=2MB --mem-type=DDR3_1600_8x8 " +\
 			"--l1d_repl=%s --l2_repl=%s --l1d_assoc=%s --l2_assoc=%s") 
 		inst = inst % (prog_name, repl, prog_loc, freq, repl, repl, assoc, assoc)
--- policy.py
+++ policy.py
@@ -1,6 +1,5 @@
 import subprocess
 
-freqs = ['1GHz', '4GHz']
 benchmarks = ['merge', 'lfsr', 'mm', 'sieve', 'spmv']
 cache_repls = ['LIPRP', 'NMRURP', 'RandomRP']
 
@@ -8,7 +7,7 @@ for prog_name in benchmarks:
     prog_loc = "../cs251a-microbench/" + prog_name
     for repl in cache_repls:
         freq = "2.2GHz"
-        inst = ("../gem5/build/X86/gem5.opt --outdir=policy/%s_%s ../gem5/configs/example/se.py --cmd=%s --cpu-type=DerivO3CPU " +\
+        inst = ("../gem5/build/X86/gem5.opt --outdir=policy_tests_%s ../gem5/configs/example/se.py --cmd=%s --cpu-type=DerivO3CPU " +\
         "--cpu-clock=%s --caches --l1d_size=64kB --l1i_size=64kB --l2cache --l2_size=2MB --mem-type=DDR3_1600_8x8 " +\
         "--l1d_repl=%s --l2_repl=%s --l1d_assoc=%s --l2_assoc=%s")
         inst = inst % (prog_name, repl, prog_loc, freq, repl, repl, 4, 4)
